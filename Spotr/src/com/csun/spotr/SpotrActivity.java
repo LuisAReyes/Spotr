301084431e48bbbe14869acf4bba02803d33cf05
chan <ngocchan.nguyen.61@my.csun.edu>
2011-11-09 14:01:05 -0800

    Something Else

--- Spotr/src/com/csun/spotr/SpotrActivity.java
+++ Spotr/src/com/csun/spotr/SpotrActivity.java
@@ -10,4 +10,8 @@ public class SpotrActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
     }
+    public void doSomething() {
+    
+    }
+    // comment
 }
