96467136eb5cc45928fc14d490e91615c94b8a03
tutran26 <tu.tran.247@my.csun.edu>
2011-11-09 22:56:47 -0800

    I've done LogIn Activity, SignUp Activity, both are full-functionally working. 11/09/2011

--- Spotr/src/com/csun/spotr/LoginActivity.java
+++ Spotr/src/com/csun/spotr/LoginActivity.java
@@ -1,108 +1,193 @@
 package com.csun.spotr;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.URI;
-import java.net.URISyntaxException;
+import java.util.ArrayList;
 
-import org.apache.http.HttpRequest;
+import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
-import org.apache.http.client.ClientProtocolException;
+import org.apache.http.NameValuePair;
 import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.impl.conn.DefaultClientConnection;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputType;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
-import android.app.Activity;
-import android.content.*;
+import android.widget.Toast;
 
 public class LoginActivity extends Activity {
-	Button userLogin;
-	EditText passwordBox;
-	EditText usernameBox;
+	public static final String TAG = "LoginActivity";
+	CheckBox checkVisible;
+	EditText edittextPassword;
+	EditText edittextUsername;
+	Button buttonLogin,buttonSignup;
+	
+	boolean passwordVisible = false;
+	
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+        
+        // Tu Tran was here 11/9/2011
+        checkVisible = (CheckBox)findViewById(R.id.login_xml_checkbox_visible_characters);
+        edittextPassword = (EditText)findViewById(R.id.login_xml_edittext_password_id);
+        edittextPassword.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+        edittextUsername = (EditText)findViewById(R.id.login_xml_edittext_email_id);
+        buttonLogin = (Button)findViewById(R.id.login_xml_button_login);
+        buttonSignup = (Button)findViewById(R.id.login_xml_button_signup);
+        
+        checkVisible.setOnClickListener(new OnClickListener() {
 
-	/** Called when the activity is first created. */
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.login);
-
-		userLogin = (Button) findViewById(R.id.login_xml_button_login);
-		passwordBox = (EditText) findViewById(R.id.login_xml_edittext_email_id);
-		usernameBox = (EditText) findViewById(R.id.login_xml_edittext_email_id);
-		userLogin.setOnClickListener(new OnClickListener() {
 			public void onClick(View v) {
-				String username = usernameBox.getText().toString();
-				Log.d("SpotrActivity", username);
-				String password = passwordBox.getText().toString();
-				Log.d("SpotrActivity", password);
-
-				HttpClient client = new DefaultHttpClient();
-				HttpRequest request = null;
-				HttpResponse response = null;
-
-				try {
-					request = new HttpGet(new URI("http://107.22.209.62/android/login"));
-
+				
+				if(passwordVisible){
+					edittextPassword.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+					passwordVisible = false;
 				}
-				catch (URISyntaxException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
+				else {
+					edittextPassword.setInputType(InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+					passwordVisible = true;
 				}
-
-				try {
-					response = client.execute((HttpUriRequest) request);
-
-				}
-				catch (ClientProtocolException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				finally {
-				}
-
-				InputStream in = null;
-				try {
-					in = response.getEntity().getContent();
-				}
-				catch (IllegalStateException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-
-				BufferedReader reader = new BufferedReader(new InputStreamReader(in));
-				StringBuilder str = new StringBuilder();
-				String line = null;
-				try {
-					while ((line = reader.readLine()) != null) {
-						str.append(line);
-					}
-				}
-				catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				String html = new String("");
-				html = str.toString();
-				Log.d("Http", html);
 			}
-		});
+        	
+        });
+        	
+        buttonLogin.setOnClickListener(new OnClickListener() {
+        	InputStream is;
+        	String url = "http://107.22.209.62/android/login.php";
+        	
+			
+			public void onClick(View v) {
+				// get data from login Activity
+				String username = edittextUsername.getText().toString();
+				String password = edittextPassword.getText().toString();
+				String result = "";
+				
+				
+				//Add data into ValuePair format
+				ArrayList<NameValuePair> loginData = new ArrayList<NameValuePair>(2);
+				loginData.add(new BasicNameValuePair("username", username));
+				loginData.add(new BasicNameValuePair("password", password));
+				
+				try 
+				{
+					//Connect via HTTP -> Encode data -> Send data
+					HttpClient httpclient = new DefaultHttpClient();
+					HttpPost httppost = new HttpPost(url);
+					httppost.setEntity(new UrlEncodedFormEntity(loginData));
+					HttpResponse response = httpclient.execute(httppost);
+					
+					//get response data
+					HttpEntity entity = response.getEntity();
+					is = entity.getContent();
+					Log.i("postdata", response.getStatusLine().toString());
+					
+				}
+				catch (Exception e)
+				{
+					Log.e("log_tag", "Error: " + e.toString());
+					//Toast.makeText(getApplicationContext(), "fail sending data", Toast.LENGTH_LONG).show();
+				}	
+				
+					//convert response to string
+				try
+				{
+					BufferedReader reader = new BufferedReader(new InputStreamReader(is,"iso-8859-1"),10);
+					StringBuilder sb = new StringBuilder();
+					String line = null;
+					while ((line = reader.readLine()) != null)
+					{
+						sb.append(line + "\n");
+					}
+					is.close();
+					result = sb.toString();
+				}
+				catch (Exception e)
+				{
+					Log.e("log_tag", "Error: " + e.toString());
+					//Toast.makeText(getApplicationContext(), "fail getting data", Toast.LENGTH_LONG).show();
+				}
+				
+				//decode json data
+				
+				String ct_name = null;
+				
+				
+				try
+				{					
+					{						
+						JSONArray jArray = new JSONArray(result);
+						if (jArray.length() > 0)
+						{							
+							JSONObject jObject = jArray.getJSONObject(0);
+							ct_name = jObject.getString("username");
+							//Toast.makeText(getApplicationContext(),"Welcome back, " + ct_name, Toast.LENGTH_LONG).show();
+							startActivity(new Intent("com.csun.spotr.MainMenuActivity"));
+						}
+						
+					}
+					
+				}
+			
+				catch (Exception e)
+				{
+					Log.e("log_tag","Error: " + e.toString());
+					showDialog(0);				
+					//Toast.makeText(getApplicationContext(),"Invalid  ", Toast.LENGTH_LONG).show();
+					//Intent i = new Intent("com.csun.spotr.SignupActivity");
+					//startActivity(i);
+					
+				}
+							
+			}
+			
+        });
+        buttonSignup.setOnClickListener(new OnClickListener() {
+        	public void onClick (View v) {
+        		startActivity(new Intent("com.csun.spotr.SignupActivity"));
+        	}
+        });
+    }
+    
+    @Override
+	protected Dialog onCreateDialog(int id) {
+		switch (id) {
+		case 0:
+			return new AlertDialog.Builder(this)
+			.setIcon(R.drawable.ic_launcher)
+			.setTitle("Error Message")
+			.setMessage("Invalid Username/Password.\n Please try again.")
+			.setPositiveButton("OK",new DialogInterface.OnClickListener() {
+				public void onClick(DialogInterface dialog,int whichButton)
+				{
+					
+				}
+			})
+			.create();
+		}
+		return null;
 	}
+    
+    public void onPause() {
+    	super.onPause();
+    	finish();
+    }
+    
 }
