70dc5e08b26b25d8ebc6546eaa558e6001c12907
Edgardo Campos <iridion3d@gmail.com>
2011-11-14 20:23:12 -0800

    I altered leaderboard.xml to reflect the fact that it needed two linear layouts and within each linear layout a list view.
    Altered leaderboard item, just slightly.
    
    Added an additional user to LeaderboardActivity and code that allows
    that additional user to be displayed in the second list view
    
    Added new images.

--- Spotr/src/com/csun/spotr/LeaderboardActivity.java
+++ Spotr/src/com/csun/spotr/LeaderboardActivity.java
@@ -12,6 +12,8 @@ import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
+
 
 import com.csun.spotr.gui.LeaderboardItemAdapter;
 
@@ -25,19 +27,46 @@ public class LeaderboardActivity extends Activity {
 	private static final String statistics[] = new String[FINAL];
 	private static final String rank[] = new String[FINAL];
 	private static final int imageId[] = new int[FINAL];
+	
+	private ListView userView;
+	private LeaderboardItemAdapter userItem;
+	
+	private static final String user[] = {"Megaman"};
+	private static final String user_stat[] = {"Super Fighting Robot"};
+	private static final String user_rank[] = {"27"}; 
+	private static final int user_imageId[] = { R.drawable.megaman };
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
 		setContentView(R.layout.leaderboard);
-
 		for (int i = 0; i < FINAL; i++) {
 			username[i] = "username" + i;
 			statistics[i] = "Status" + i;
 			rank[i] = "" + (i + 1);
-			imageId[i] = R.drawable.leaderboard2;
+			imageId[i] = R.drawable.leaderboard;
 		}
 		
+		////////////////////////////////////////////////////////////////////
+		userView = (ListView) findViewById(R.id.leaderboard_xml_user_view);
+		userItem = new LeaderboardItemAdapter(this,user, user_stat, user_rank, user_imageId);
+		userView.setAdapter(userItem);
+		userView.setOnItemClickListener(new OnItemClickListener() {
+			public void onItemClick(AdapterView<?> parent, View view, int position, long id) 
+			{
+				//-- Edgardo C. -- //
+				// Added a makeText generator to show name.
+				// Will later implement to show score, possibly?
+				Toast.makeText(getBaseContext(),
+				user[position] + "'s Score Goes Here", Toast.LENGTH_SHORT).show();
+						
+			}
+		});
+		
+		///////////////////////////////////////////////////////////////////////////
+		
+
 		leaderboardListView = (ListView) findViewById(R.id.leaderboard_xml_listview);
 		leaderboardItems = new LeaderboardItemAdapter(this, username, statistics, rank, imageId);
 		leaderboardListView.setAdapter(leaderboardItems);
