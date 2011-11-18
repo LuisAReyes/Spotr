ff31a3ecbcb774d7a3db82c45ae502d06b41fb18
johnandrewdavid <johnandrew.david.318@my.csun.edu>
2011-11-18 13:25:00 -0800

    Added RewardImageAdapter.java, icons. Updated reward.xml, RewardActivity.java.

--- Spotr/src/com/csun/spotr/RewardActivity.java
+++ Spotr/src/com/csun/spotr/RewardActivity.java
@@ -1,12 +1,56 @@
 package com.csun.spotr;
 
+import com.csun.spotr.gui.RewardImageAdapter;
+
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.GridView;
+import android.widget.Toast;
+import android.widget.AdapterView.OnItemClickListener;
 
 public class RewardActivity extends Activity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.reward);
+		
+		Button btnMainMenu, btnSetting;
+		
+		//button for Main Menu
+		btnMainMenu = (Button) findViewById(R.id.reward_xml_button_mainmenu);
+		btnMainMenu.setOnClickListener(new OnClickListener() {
+			public void onClick(View arg0) {
+				Intent i = new Intent("com.csun.spotr.MainMenuActivity");
+				startActivity(i);
+			}
+		});
+		
+		//button for Setting Menu
+		btnSetting = (Button) findViewById(R.id.reward_xml_button_setting);
+		btnSetting.setOnClickListener(new OnClickListener() {
+			public void onClick(View arg0) {
+				Intent i = new Intent("com.csun.spotr.MainMenuActivity");
+				startActivity(i);
+			}
+		});
+		
+		//GridView
+	    GridView gridview = (GridView) findViewById(R.id.reward_xml_gridView1);
+	    gridview.setAdapter(new RewardImageAdapter(this));
+
+	    gridview.setOnItemClickListener(new OnItemClickListener() {
+	        public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+	            Toast.makeText(RewardActivity.this, "Reward" + position, Toast.LENGTH_SHORT).show();
+	            
+	        }
+	    });
+		
+
+		
 	}
 }
