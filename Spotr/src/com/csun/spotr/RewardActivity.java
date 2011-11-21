c73a0964a75331dc2354e2863b12c667737cfbf1
johnandrewdavid <johnandrew.david.318@my.csun.edu>
2011-11-20 20:28:48 -0800

    Added RewardViewActivity.java, reward_view.xml. Updated RewardActivity.java, reward.xml, AndroidManifest.xml

--- Spotr/src/com/csun/spotr/RewardActivity.java
+++ Spotr/src/com/csun/spotr/RewardActivity.java
@@ -3,13 +3,20 @@ package com.csun.spotr;
 import com.csun.spotr.gui.RewardImageAdapter;
 
 import android.app.Activity;
+import android.app.Dialog;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.AdapterView.OnItemClickListener;
 
@@ -19,20 +26,11 @@ public class RewardActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.reward);
 		
-		Button btnMainMenu, btnSetting;
+		Button btnMainMenu;
 		
-		//button for Main Menu
+		//Button for Main Menu
 		btnMainMenu = (Button) findViewById(R.id.reward_xml_button_mainmenu);
 		btnMainMenu.setOnClickListener(new OnClickListener() {
-			public void onClick(View arg0) {
-				Intent i = new Intent("com.csun.spotr.MainMenuActivity");
-				startActivity(i);
-			}
-		});
-		
-		//button for Setting Menu
-		btnSetting = (Button) findViewById(R.id.reward_xml_button_setting);
-		btnSetting.setOnClickListener(new OnClickListener() {
 			public void onClick(View arg0) {
 				Intent i = new Intent("com.csun.spotr.MainMenuActivity");
 				startActivity(i);
@@ -45,12 +43,38 @@ public class RewardActivity extends Activity {
 
 	    gridview.setOnItemClickListener(new OnItemClickListener() {
 	        public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
-	            Toast.makeText(RewardActivity.this, "Reward" + position, Toast.LENGTH_SHORT).show();
-	            
-	        }
-	    });
-		
+	           //Toast.makeText(RewardActivity.this, "Reward " + position, Toast.LENGTH_SHORT).show();
+	        	
+	           Intent intent = new Intent(getApplicationContext(), RewardViewActivity.class);
+	           startActivity(intent);
 
-		
+	        }
+	    });	
 	}
+	
+	
+	//Menu Button Selections
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.options_menu, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.options_menu_xml_item_setting_Icon :
+			Intent i = new Intent("com.csun.spotr.SettingsActivity");
+			startActivity(i);
+			break;
+		case R.id.options_menu_xml_item_logout_Icon :
+			Intent k = new Intent("com.csun.spotr.LoginActivity");
+			startActivity(k);
+			break;
+		}
+		return true;
+	}
+	
+	
 }
---
+++ Spotr/src/com/csun/spotr/RewardViewActivity.java
@@ -0,0 +1,21 @@
+package com.csun.spotr;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class RewardViewActivity extends Activity {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.reward_view);
+		
+	}
+}
