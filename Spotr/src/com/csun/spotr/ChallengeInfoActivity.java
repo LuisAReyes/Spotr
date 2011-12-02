47b1a4b60524458569d02b83eacd44959d0b1d1b
LuigiMarioKOG <luigimariokog@gmail.com>
2011-12-02 00:15:01 -0800

    Success! Challenge info screen finally works.

--- Spotr/src/com/csun/spotr/ChallengeInfoActivity.java
+++ Spotr/src/com/csun/spotr/ChallengeInfoActivity.java
@@ -5,6 +5,7 @@
 
 package com.csun.spotr;
 
+import com.csun.spotr.core.CurrentChallenge;
 import com.csun.spotr.gui.ChallengeInfoAdapter;
 
 import android.app.Activity;
@@ -16,12 +17,14 @@ import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.CheckBox;
 import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.RatingBar;
 import android.widget.TextView;
 import android.widget.AdapterView.OnItemClickListener;
 
 public class ChallengeInfoActivity extends Activity {
-	private ChallengeInfoAdapter challengeInfo;
-	private ListView challengeInfoListView;
+	//private ChallengeInfoAdapter challengeInfo;
+	//private ListView challengeInfoListView;
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -31,13 +34,44 @@ public class ChallengeInfoActivity extends Activity {
 	@Override
 	public void onStart() {
 		super.onStart();
-		challengeInfoListView = new ListView(this);
+		/*challengeInfoListView = new ListView(this);
 		challengeInfo = new ChallengeInfoAdapter(this);
 		challengeInfoListView.setAdapter(challengeInfo);
 		challengeInfoListView.setOnItemClickListener(new OnItemClickListener() {
 			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 				
 			}
-		});
+		});*/
+		
+		TextView nameTextView = (TextView)findViewById(R.id.challenge_info_xml_textview_name);
+		CheckBox flagCheckBox = (CheckBox)findViewById(R.id.challenge_info_xml_checkbox_reviewflag);
+		TextView descriptionTextView = (TextView)findViewById(R.id.challenge_info_xml_textview_description);
+		RatingBar avgratingRatingBar = (RatingBar)findViewById(R.id.challenge_info_xml_ratingbar_rating);
+		ProgressBar progressProgressBar = (ProgressBar)findViewById(R.id.challenge_info_xml_progressbar_progress);
+		TextView locationTextView = (TextView)findViewById(R.id.challenge_info_xml_textview_location);
+		TextView pointsTextView = (TextView)findViewById(R.id.challenge_info_xml_textview_points);
+		TextView avgratingTextView = (TextView)findViewById(R.id.challenge_info_xml_textview_avgrating);
+		TextView attemptedTextView = (TextView)findViewById(R.id.challenge_info_xml_textview_attempted);
+		TextView completedTextView = (TextView)findViewById(R.id.challenge_info_xml_textview_completed);
+		TextView categoryTextView = (TextView)findViewById(R.id.challenge_info_xml_textview_category);
+		
+		
+		nameTextView.setText(CurrentChallenge.name);
+		descriptionTextView.setText(CurrentChallenge.description);
+		flagCheckBox.setChecked(CurrentChallenge.reviewFlag);
+		avgratingRatingBar.setRating(CurrentChallenge.averageRating);
+		if(-1 == CurrentChallenge.progress)
+			progressProgressBar.setProgress(0);
+		else
+			progressProgressBar.setProgress(CurrentChallenge.progress);
+		locationTextView.setText(CurrentChallenge.location);
+		pointsTextView.setText(Integer.toString(CurrentChallenge.points));
+		avgratingTextView.setText(Float.toString(CurrentChallenge.averageRating));
+		attemptedTextView.setText(Integer.toString(CurrentChallenge.numStarted));
+		completedTextView.setText(Integer.toString(CurrentChallenge.numCompleted));
+		String category = "Categories: " + CurrentChallenge.category[0];
+		for(int i = 1; i < CurrentChallenge.category.length; i++)
+			category += ", " + CurrentChallenge.category[i];
+		categoryTextView.setText(category);
 	}
 }
