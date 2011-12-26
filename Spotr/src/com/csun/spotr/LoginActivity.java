6d3a6206290b63f61a3f380a3cb4e16446becff4
chan <atbl1511@hotmail.com>
2011-12-26 10:20:13 -0800

    Change LoginActivity to use AsyncTask for Http connection.

--- Spotr/src/com/csun/spotr/LoginActivity.java
+++ Spotr/src/com/csun/spotr/LoginActivity.java
@@ -1,22 +1,16 @@
 package com.csun.spotr;
 
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.List;
 
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
 import com.csun.spotr.core.CurrentUser;
+import com.csun.spotr.core.User;
+import com.csun.spotr.helper.JsonHelper;
 
 import android.app.Activity;
 import android.app.AlertDialog;
@@ -25,56 +19,44 @@ import android.content.DialogInterface;
 import android.content.SharedPreferences;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.text.InputType;
-import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
-import android.widget.Toast;
 
 public class LoginActivity extends Activity {
-	public static final String TAG = "[LoginActivity]";
+	private static final String TAG = "[LoginActivity]";
+	private static final String LOGIN_URL = "http://107.22.209.62/android/login.php";
 
-	CheckBox checkVisible;
-	CheckBox checkSavePassword;
-	EditText edittextPassword;
-	EditText edittextUsername;
-	Button buttonLogin;
-	Button buttonSignup;
-
-	// Zach Duvall (11/15/2011)
-	SharedPreferences prefs;
-	SharedPreferences.Editor editor;
-	boolean prefsSavePassword;
-	String prefsUsername;
-	String prefsPassword;
-
-	boolean passwordVisible = false;
-	boolean savePassword = false;
+	private EditText edittextUsername;
+	private EditText edittextPassword;
+	private SharedPreferences prefs;
+	private SharedPreferences.Editor editor;
+	private boolean prefsSavePassword;
+	private boolean passwordVisible = false;
+	private boolean savePassword = false;
 
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.login);
 
-		// Zach Duvall (11/15/2011)
 		prefs = getSharedPreferences("Spotr", MODE_PRIVATE);
 		prefsSavePassword = prefs.getBoolean("savePassword", false);
-		prefsUsername = prefs.getString("username", "");
-		prefsPassword = prefs.getString("password", "");
+		String prefsUsername = prefs.getString("username", "");
+		String prefsPassword = prefs.getString("password", "");
 
-		// Tu Tran was here 11/9/2011
-		checkVisible = (CheckBox) findViewById(R.id.login_xml_checkbox_visible_characters);
-		checkSavePassword = (CheckBox) findViewById(R.id.login_xml_checkbox_remember_password);
+		CheckBox checkVisible = (CheckBox) findViewById(R.id.login_xml_checkbox_visible_characters);
+		CheckBox checkSavePassword = (CheckBox) findViewById(R.id.login_xml_checkbox_remember_password);
 		edittextPassword = (EditText) findViewById(R.id.login_xml_edittext_password_id);
 		edittextPassword.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
 		edittextUsername = (EditText) findViewById(R.id.login_xml_edittext_email_id);
-		buttonLogin = (Button) findViewById(R.id.login_xml_button_login);
-		buttonSignup = (Button) findViewById(R.id.login_xml_button_signup);
+		Button buttonLogin = (Button) findViewById(R.id.login_xml_button_login);
+		Button buttonSignup = (Button) findViewById(R.id.login_xml_button_signup);
 
-		// Zach Duvall (11/15/2011)
 		if (prefsSavePassword) {
 			edittextUsername.append(prefsUsername);
 			edittextPassword.append(prefsPassword);
@@ -95,92 +77,18 @@ public class LoginActivity extends Activity {
 		});
 
 		buttonLogin.setOnClickListener(new OnClickListener() {
-			InputStream is;
-			String url = "http://107.22.209.62/android/login.php";
-
 			public void onClick(View v) {
-				// get data from login Activity
-				String username = edittextUsername.getText().toString();
-				String password = edittextPassword.getText().toString();
-				String result = "";
-
-				// Add data into ValuePair format
-				ArrayList<NameValuePair> loginData = new ArrayList<NameValuePair>(2);
-				loginData.add(new BasicNameValuePair("username", username));
-				loginData.add(new BasicNameValuePair("password", password));
-
-				try {
-					// Connect via HTTP -> Encode data -> Send data
-					HttpClient httpclient = new DefaultHttpClient();
-					HttpPost httppost = new HttpPost(url);
-					httppost.setEntity(new UrlEncodedFormEntity(loginData));
-					HttpResponse response = httpclient.execute(httppost);
-
-					// get response data
-					HttpEntity entity = response.getEntity();
-					is = entity.getContent();
-					Log.i("postdata", response.getStatusLine().toString());
-
-				}
-				catch (Exception e) {
-					Log.e(TAG, "Error: " + e.toString());
-				}
-
-				// convert response to string
-				try {
-					BufferedReader reader = new BufferedReader(new InputStreamReader(is, "iso-8859-1"), 10);
-					StringBuilder sb = new StringBuilder();
-					String line = null;
-					while ((line = reader.readLine()) != null) {
-						sb.append(line + "\n");
-					}
-					is.close();
-					result = sb.toString();
-				}
-				catch (Exception e) {
-					Log.e(TAG, "Error: " + e.toString());
-				}
-
-				// decode json data
-				try {
-					{
-						JSONArray jArray = new JSONArray(result);
-						if (jArray.length() > 0) {
-							JSONObject jObject = jArray.getJSONObject(0);
-							String userName = jObject.getString("username");
-
-							// Zach Duvall (11/15/2011)
-							if (savePassword) {
-								editor = getSharedPreferences("Spotr", MODE_PRIVATE).edit();
-								editor.putBoolean("savePassword", true);
-								editor.putString("username", username);
-								editor.putString("password", password);
-								editor.commit();
-							}
-							
-							CurrentUser.setCurrentUser(23, username, password);
-							startActivity(new Intent("com.csun.spotr.MainMenuActivity"));
-						}
-
-					}
-
-				}
-
-				catch (Exception e) {
-					Log.e(TAG, "Error: " + e.toString());
-					showDialog(0);
-				}
-
+				LoginTask task = new LoginTask();
+				task.execute();
 			}
-
 		});
+		
 		buttonSignup.setOnClickListener(new OnClickListener() {
 			public void onClick(View v) {
 				startActivity(new Intent("com.csun.spotr.SignupActivity"));
 			}
 		});
 
-		// Zach Duvall (11/15/2011)
 		checkSavePassword.setOnClickListener(new OnClickListener() {
 			public void onClick(View v) {
 				if (!savePassword)
@@ -203,6 +111,7 @@ public class LoginActivity extends Activity {
 					.setMessage("Invalid Username/Password.\n Please try again.")
 					.setPositiveButton("OK", new DialogInterface.OnClickListener() {
 						public void onClick(DialogInterface dialog, int whichButton) {
+							
 						}
 					}).create();
 		}
@@ -212,6 +121,57 @@ public class LoginActivity extends Activity {
 	public void onPause() {
 		super.onPause();
 		finish();
+	}
+	
+	private class LoginTask extends AsyncTask<Void, Integer, Boolean> {
+		private List<NameValuePair> loginData = new ArrayList<NameValuePair>();
+		@Override
+		protected void onPreExecute() {
+			// make a pair of data for HttpPost 
+			loginData.add(new BasicNameValuePair("username", edittextUsername.getText().toString()));
+			loginData.add(new BasicNameValuePair("password", edittextPassword.getText().toString()));
+		}
+		
+		@Override
+		protected Boolean doInBackground(Void... unused) {
+			// get data from the our database 
+			JSONArray array = JsonHelper.getJsonArrayFromUrlWithData(LOGIN_URL, loginData);
+			try {
+				JSONObject jsonUserInfo = array.getJSONObject(0);
+				// attempt to get data
+				jsonUserInfo.getString("username");
+				if (savePassword) {
+					editor = getSharedPreferences("Spotr", MODE_PRIVATE).edit();
+					editor.putBoolean("savePassword", true);
+					editor.putString("username", jsonUserInfo.getString("username"));
+					editor.putString("password", jsonUserInfo.getString("password"));
+					editor.commit();
+				
+					// set the current user
+					CurrentUser.setCurrentUser(23, jsonUserInfo.getString("username"), jsonUserInfo.getString("password"));
+				}
+			}
+			catch (Exception e) {
+				showDialog(0);
+				return false; 
+			}
+			return true;
+		}
+
+		protected void onProgressUpdate(Integer... progress) {
+			// do nothing
+		}
+		
+		@Override
+		protected void onPostExecute(Boolean result) {
+			if (result == false) {
+				showDialog(0);
+			}
+			else {
+				User user = CurrentUser.getCurrentUser();
+				startActivity(new Intent("com.csun.spotr.MainMenuActivity"));
+			}
+		}
 	}
 
 }
