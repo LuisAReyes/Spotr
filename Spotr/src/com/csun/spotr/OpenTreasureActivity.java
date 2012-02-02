a0ef3ed24e57b972de87ca76cfffee8094c617a6
chan <ngocchan.nguyen.61@my.csun.edu>
2012-02-01 20:43:09 -0800

    Add TreasureActivity/WeaponActivity.

---
+++ Spotr/src/com/csun/spotr/OpenTreasureActivity.java
@@ -0,0 +1,13 @@
+package com.csun.spotr;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class OpenTreasureActivity extends Activity {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		// set layout
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.place_action);
+	}
+}
